6b1d7fa923fef18ebc261877416fe91c25f2a7b5
Lionel <lionel@lightly.ai>
2025-07-05 23:02:02 +0200

    push new things

--- content/posts/2025-06-30_torch-fsdp/ddp_vs_fsdp.py
+++ content/posts/2025-06-30_torch-fsdp/ddp_vs_fsdp.py
@@ -1,16 +1,17 @@
 import os
 import contextlib
-import logging
 
 import torch
 from torch.optim import Adam
 import torch.nn.functional as F
 import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.distributed as dist
-from torch.nn import Module
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.nn import Linear
+from torch.distributed.fsdp.wrap import ModuleWrapPolicy
 
-from linear_model import LinearModel
+from mlp import MLPModel
 
 MASTER_ADDR = "localhost"
 MASTER_PORT = "12355"
@@ -30,14 +31,32 @@ def setup_dist(rank: int, world_size: int):
     finally:
         dist.destroy_process_group()
 
-def main(rank: int, world_size: int):
-    param_size = 8192 * 2
+def main(rank: int, world_size: int, fsdp: bool):
+    param_size = 8192
+    num_layers = 5
     with setup_dist(rank, world_size):
+        device = torch.device(f"cuda:{rank}")
         torch.cuda.reset_peak_memory_stats()
-        model = DDP(
-            LinearModel(input_size=param_size, output_size=param_size).to(rank)
+        torch.cuda.set_device(rank)
+
+        model = MLPModel(
+            input_size=param_size, 
+            output_size=param_size, 
+            hidden_size=param_size, 
+            num_layers=num_layers,
         )
-        optimizer = Adam(model.parameters(), lr=1e-1)
+        if fsdp:
+            model = FSDP(
+                model,
+                auto_wrap_policy=ModuleWrapPolicy({Linear}),
+                device_id=device,
+            )
+        else:
+            model = DDP(
+                model.to(device),
+            )
+
+        optimizer = Adam(model.parameters(), lr=1e-3)
 
         y = torch.randn(256, param_size).to(rank)
         x = torch.randn(256, param_size).to(rank)
@@ -50,9 +69,15 @@ def main(rank: int, world_size: int):
             optimizer.step()
 
             print(f"Epoch {i+1}, Loss: {loss.item():.4f}")
-
+        
         print(f"Max memory allocated on rank {rank}: {torch.cuda.max_memory_allocated(device=rank) / (1024 ** 2):.2f} MB")
 
 if __name__ == "__main__":
-    world_size = 2
-    mp.spawn(main, args=(world_size,), nprocs=world_size)
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('--fsdp', action='store_true', help='Use FSDP instead of DDP')
+    args = parser.parse_args()
+
+    world_size = 4
+    mp.spawn(main, args=(world_size, args.fsdp), nprocs=world_size)
